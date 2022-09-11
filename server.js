455d51df7da9c250ea78dbe7851b8755740265f4
David Stavis <davidstavis@Davids-MacBook-Pro.local>
2022-09-10 20:56:44 -0600

    Remove reference to movieData.json

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-// const movieData = require("./movieData.json")  
-
 app.use(cors());
 app.use(express.json());
 
@@ -22,15 +20,15 @@ app.locals.counter = 1;
 app.locals.prescriptions = [];
 
 app.post("/prescriptions", (request, response) => {
-  const { message, movieID } = request.body
-  if(!message || !movieID) {
-    response.status(422).json( { error: "Expected { message: <String>, movieID: <Number>}" } )
+  const { message, showID } = request.body
+  if(!message || !showID) {
+    response.status(422).json( { error: "Expected { message: <String>, showID: <Number>}" } )
   }
 
   const newPrescription = {
     id: app.locals.counter,
     message: message,
-    movieID: movieID
+    showID: showID
   }
   app.locals.counter ++
 
@@ -40,12 +38,8 @@ app.post("/prescriptions", (request, response) => {
 
 app.get('/prescriptions/:id', (request, response) => {
     const { id } = request.params
-    // console.log(request.params)
-    // console.log("counter", app.locals.counter)
-    // console.log("prescriptions", app.locals.prescriptions)
     
     const requestedPrescription = app.locals.prescriptions.find( (prescription) => {
-      // console.log("find prescription.id ", prescription.id)
       return prescription.id == id;
     } )
     console.log(requestedPrescription)
