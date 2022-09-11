ef42c78fe76fddb6906fa26703e6d0687a7a5708
David Stavis <davidstavis@Davids-MacBook-Pro.local>
2022-09-10 19:54:13 -0600

    Prescription post endpoint persists prescription data to app.locals, and returns that prescription with a unique id

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const movieData = require("./movieData.json")  
 const app = express();
 const cors = require('cors')
 
+// const movieData = require("./movieData.json")  
+
 app.use(cors());
+app.use(express.json());
 
 app.set('port', process.env.PORT || 3002);
-app.locals.title = 'Genre API';
+app.locals.title = 'TasteRX API';
 
 app.get('/', (request, response) => {
   response.send('TasteRX API');
@@ -16,10 +18,28 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
 
-app.locals;
+app.locals.counter = 1;
+app.locals.prescriptions = [];
 
-// app.get('/movies', (request, response) => {
-//     const movies = app.locals.movies
-//     response.status(200).json(movies);
-//   });
+app.post("/prescriptions", (request, response) => {
+  const { message, movieID } = request.body
+  if(!message || !movieID) {
+    response.status(422).json( { error: "Expected { message: <String>, movieID: <Number>}" } )
+  }
+
+  const newPrescription = {
+    id: app.locals.counter,
+    message: message,
+    movieID: movieID
+  }
+  app.locals.counter ++
+
+  app.locals.prescriptions.push(newPrescription)
+  response.status(201).json({ prescription: newPrescription})
+})
+
+app.get('/prescriptions/:id', (request, response) => {
+    // const prescriptions = app.locals.prescriptions;
+    // response.status(200).json(prescriptions);
+  });
 
