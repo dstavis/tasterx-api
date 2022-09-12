d677ccf6501052f90f85252c4eb5af93b021ef9f
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-12 09:53:54 -0600

    Ready to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-app.use(cors());
+app.use(cors()); //can put url into the cors()
 app.use(express.json());
 
 app.set('port', process.env.PORT || 3002);
