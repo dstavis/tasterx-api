28b4171585205f93006cbe8f930fc3cdc05c97f9
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-12 11:02:47 -0600

    Begin sequelize configuration

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "production": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres"
+  }
+}
+
+
+
+
