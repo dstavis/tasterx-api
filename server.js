fa7c953d3664a0850ae7a82b561a0dd0f1250329
Anna Spitz <aspitzemail@gmail.com>
2022-09-13 20:33:30 -0500

    Change data back

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/prescriptions/:id', async (request, response) => {
     if (requestedPrescription === null) {
       response.status(422).json( { error: "Not found" } )
     } else {
-      response.status(201).json(requestedPrescription);
+      response.status(201).json({ data: requestedPrescription });
     }
   });
 
