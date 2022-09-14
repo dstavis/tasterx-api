410d22c3f3ba696c9cafd9758d7fc2644d5bd236
David Stavis <davidstavis@gmail.com>
2022-09-14 15:01:32 -0600

    Refactor url endpoints to follow REST conventions, remove reference to localhost, prevent crashes, add error handling for prescriptions get route, update error messages for usefulness, changed response format for prescriptions post

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { sequelize, models } = require('./model/index');
 const { response } = require('express');
 
-app.use(cors()); //can put url into the cors()
+app.use(cors());
 app.use(express.json());
 
 app.set('port', process.env.PORT || 3002);
@@ -15,33 +15,38 @@ app.get('/', (request, response) => {
   response.send('TasteRX API');
 });
 
+app.get('/prescriptions', async (request, response) => {
+  const allRX = await models.TVPrescription.findAll();
+  if(!allRX) {
+    response.status(422).json( { error: "No prescriptions found" } )
+  } else {
+    response.status(200).json(allRX);
+  }
+});
+
 app.post("/prescriptions", async (request, response) => {
   const { message, showID } = request.body
   if(!message || !showID) {
     response.status(422).json( { error: "Expected { message: <String>, showID: <Number> }" } )
+  } else {
+    const newPrescription = await models.TVPrescription.create({ message: message, showID: showID })
+    response.status(201).json({ prescription: newPrescription })
   }
-  const newPrescription = await models.TVPrescription.create({ message: message, showID: showID })
-  response.status(201).json({ prescription: newPrescription })
 })
 
 app.get('/prescriptions/:id', async (request, response) => {
     const { id } = request.params
     const requestedPrescription = await models.TVPrescription.findOne({ where: { id: id } });
-    console.log(requestedPrescription)
     if (requestedPrescription === null) {
-      response.status(422).json( { error: "Not found" } )
+      response.status(422).json( { error: "Prescription with the given ID was not found" } )
     } else {
-      response.status(200).json({ data: requestedPrescription });
+      response.status(200).json({ prescription: requestedPrescription });
     }
   });
 
-app.get('/all', async (request, response) => {
-  const allRX = await models.TVPrescription.findAll();
-  response.status(200).json(allRX);
-});
 
 sequelize.authenticate().then(() => {
   app.listen(app.get('port'), () => {
-    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+    console.log(`${app.locals.title} is running on Port ${app.get('port')}.`);
   });
 })
