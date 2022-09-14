949f7175c16d30afeaaf6f3ffc6fccc397e0736f
Anna Spitz <aspitzemail@gmail.com>
2022-09-13 20:05:11 -0500

    Add condition to dialect

--- model/index.js
+++ model/index.js
@@ -5,12 +5,12 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL,
   {
     dialect: 'postgres',
-    dialectOptions: {
+    ...(!process.env.NODE_ENV === 'production' && {dialectOptions: {
       ssl: {
         required: true,
         rejectUnauthorized: false,
       }
-    }
+    }})
   },
 );
 
